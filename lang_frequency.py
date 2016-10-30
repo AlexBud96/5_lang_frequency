7af27d230def1e98aa41fc2dc8c493d7a7ce84f0
AlexBud96 <AlexSy4ov@gmail.com>
2016-10-30 19:58:59 +0000

    Исправлен README, добавлены комментарии

--- lang_frequency.py
+++ lang_frequency.py
@@ -3,7 +3,7 @@ import sys
 import string
 import operator
 
-def load_data(filepath):
+def load_data(filepath):                # функция открывает файловый поток, считывает текст, закрывает поток и возвращает текст.
     if not os.path.exists(filepath):
         return None
     file = open(filepath, "r")
@@ -12,16 +12,16 @@ def load_data(filepath):
     return text
 
 
-def get_most_frequent_words(text):
+def get_most_frequent_words(text):              # функция преобразует текст в словарь из слов в порядке убывания частоты
     table = str.maketrans("", "")
     remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
-    words = text.lower().split(None)
+    words = text.split(None)                # word - список из единичных слов
 
     frequencies = {}
     for word in words:
-        trimmed = word.translate(remove_punctuation_map)
+        trimmed = word.translate(remove_punctuation_map)                # удаление знаков пунктуации
         if trimmed.isalpha():
-            frequencies[trimmed] = frequencies.get(trimmed, 0) + 1
+            frequencies[trimmed] = frequencies.get(trimmed, 0) + 1      # frequencies - список из слов и их количества
 
     return frequencies
 
@@ -30,6 +30,6 @@ if __name__ == '__main__':
     data_dir = input('Укажите путь до файла: ')
     text = load_data(data_dir)
     frequencies = get_most_frequent_words(text)
-    keys = sorted(frequencies.items(), key=operator.itemgetter(1), reverse=True)
+    keys = sorted(frequencies.items(), key=operator.itemgetter(1), reverse=True)    # список слов и их место по частоте использования
     for number in range(10):
             print ("%-32s %d" % (keys[number]))
