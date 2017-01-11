ad598ca8a05b51884930b51ffc2ffbd13569ca4c
AlexBud96 <AlexSy4ov@gmail.com>
2017-01-11 18:58:56 +0000

    Задание №5 devman

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,35 +1,26 @@
+import re
 import os
-import sys
-import string
-import operator
+from collections import Counter
 
-def load_data(filepath):                # функция открывает файловый поток, считывает текст, закрывает поток и возвращает текст.
+
+def load_data(filepath):
     if not os.path.exists(filepath):
         return None
-    file = open(filepath, "r")
-    text = file.read()
-    file.close()
-    return text
+    with open(filepath, 'r') as file:
+        return file.read().lower()
 
 
-def get_most_frequent_words(text):              # функция преобразует текст в словарь из слов в порядке убывания частоты
-    table = str.maketrans("", "")
-    remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
-    words = text.split(None)                # word - список из единичных слов
-
-    frequencies = {}
-    for word in words:
-        trimmed = word.translate(remove_punctuation_map)                # удаление знаков пунктуации
-        if trimmed.isalpha():
-            frequencies[trimmed] = frequencies.get(trimmed, 0) + 1      # frequencies - список из слов и их количества
-
-    return frequencies
+def lang_frequency(text):
+    words = re.findall(r'\w+', text)
+    most_common_words = 10
+    return Counter(words).most_common(most_common_words)
 
 
 if __name__ == '__main__':
-    data_dir = input('Укажите путь до файла: ')
-    text = load_data(data_dir)
-    frequencies = get_most_frequent_words(text)
-    keys = sorted(frequencies.items(), key=operator.itemgetter(1), reverse=True)    # список слов и их место по частоте использования
-    for number in range(10):
-            print ("%-32s %d" % (keys[number]))
+    filepath = input('Введите путь до файла: ')
+    text = load_data(filepath)
+    if not text:
+        print('Не удалось открыть файл')
+    else:
+        for word, frequency in lang_frequency(text):
+            print(word, frequency)
